1c0faa19bb70b1750c3fe2a79b966f7a81b24c67
Ионова Дарья <ionova.dd@edu.spbstu.ru>
2026-03-18 00:42:10 +0300

    Update task_2-5_nucl_count.py

--- projects_2/task_2-5/task_2-5_nucl_count.py
+++ projects_2/task_2-5/task_2-5_nucl_count.py
@@ -13,8 +13,11 @@ def nucleotide_count():
     len_sequence = len(sequence)
 
     return a_count, t_count, g_count, c_count, len_sequence
-
-result = nucleotide_count()
+a_con = (a_count/len_sequence)*100
+t_con = (t_count/len_sequence)*100
+g_con = (g_count/len_sequence)*100
+c_con = (c_count/len_sequence)*100
 
 print(f"\nПодсчёт нуклеотидов:\nA:\t{result[0]}\nT:\t{result[1]}\nG:\t{result[2]}\nC:\t{result[3]}")
 print(f"\nОбщая длина: {result[4]}")
+print(f"Процентное содержание каждого нуклеотида:\аденина:\t{a_con:.2f}\nтимина:\t{t_con:.2f}\nгуанина:\t{g_con:.2f}\nцитозина:\t{t_con:.2f}")
