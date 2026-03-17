202767afb2b025ffdeeacd4bffed7a01c7820b28
Ионова Дарья <ionova.dd@edu.spbstu.ru>
2026-03-18 00:30:15 +0300

    Update task_2-4_lab_assistant.py

--- projects_2/task_2-4/task_2-4_lab_assistant.py
+++ projects_2/task_2-4/task_2-4_lab_assistant.py
@@ -1,32 +1,11 @@
-print(f"--- Расчёт рецепта растовра поваренной соли при 25°C ---\n")
+volume = float(input("Введите нужный объем раствора (мл): "))
+salt_mass = volume * 0.009
 
-def count_recipe():
+with open("recipe.txt", "w", encoding="utf-8") as file:
+    file.write("ОТЧЕТ ПО ПРИГОТОВЛЕНИЮ:\n")
+    file.write("-" * 23 + "\n")
+    file.write(f"Общий объем: {volume:.2f} мл\n")
+    file.write(f"Масса соли:  {salt_mass:.2f} г\n")
+    file.write(f"Объем воды:  {volume:.2f} мл")
 
-    salt_density: float = 2.161
-    max_solution_saturation: float = 0.357
-    max_mass_fraction = 0.2631
-
-    solution_volume = float(input("Введите нужный объем раствора (мл): "))
-    req_salt_сonc = float(input("Введите нужную концентрацию раствора по массе (%): ")) / 100
-
-    if solution_volume < 0 or 1 < req_salt_сonc < 0:
-        print(f"\nПроверьте запрос.")
-        return count_recipe()
-    
-    required_salt_mass = solution_volume * ( (req_salt_сonc * salt_density) / (salt_density * (1 - req_salt_сonc) + req_salt_сonc) )
-    water_volume = required_salt_mass * (1 - req_salt_сonc) / req_salt_сonc
-
-    if required_salt_mass / water_volume > max_solution_saturation:
-        print(f"\n!!! Ваш раствор перенасыщен (максимум {max_mass_fraction*100:.1f}%) !!!")
-        print(f"\n!!! Возможно, стоит изменить исходные данные !!!")
-
-    return solution_volume, required_salt_mass, water_volume
-
-recipe = count_recipe()
-print(f"\nОТЧЕТ ПО ПРИГОТОВЛЕНИЮ:\n-----------------------\nОбщий объем:\t{recipe[0]:.2f}\nМасса соли:\t{recipe[1]:.2f}\nОбъем воды:\t{recipe[2]:.2f}")
-
-with open("C:\\Users\\timuh\\Documents\\ukhanov_tu\\projects_2\\task_2_4\\recipe.txt", "w", encoding="utf-8") as file:
-
-    file.write(f"ОТЧЕТ ПО ПРИГОТОВЛЕНИЮ:\n-----------------------\nОбщий объем:\t{recipe[0]:.2f}\nМасса соли:\t{recipe[1]:.2f}\nОбъем воды:\t{recipe[2]:.2f}")
-
-# растворимость NaCl при 25°С равна 35.7 г соли на 100 г воды
+print("Рецепт сохранен в файл recipe.txt")
