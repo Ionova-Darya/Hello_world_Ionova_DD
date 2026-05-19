c7bed6f1fa9e2363ccc0c51b5af465e4998202a2
Ионова Дарья <ionova.dd@edu.spbstu.ru>
2026-05-19 23:20:06 +0300

    Update 6-0-2.py

--- projects_6/task_6_0/6-0-2.py
+++ projects_6/task_6_0/6-0-2.py
@@ -2,6 +2,11 @@ import pandas as pd
 with open("C:/Users/Даша/Desktop/6_0_2.txt", "w") as f:
   df = pd.read_csv("C:/Users/Даша/Downloads/wild_boars.csv")
   col = list(df.columns)
-    for i in col[2:]:
-        a =  df[i].mean()
-        print(f'Boars average is {a:.2f}\n', file=f)
+  for i in col[2:]:  #прохожу по столбцам
+        srznac =  df[i].mean()
+        parts = i.split('_')
+        if len(parts) == 2:
+            param = parts[0] 
+        else:
+            param = parts[0] + ' ' + parts[1]
+        print(f'Boars average {param} is {srznac:.2f} {parts[-1]}\n', file=f)
