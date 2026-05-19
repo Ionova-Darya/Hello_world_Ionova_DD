f480f49aa60571672a5539152ab0e19b7587673a
Ионова Дарья <ionova.dd@edu.spbstu.ru>
2026-05-19 23:23:56 +0300

    Create 6-0-4.py

---
+++ projects_6/task_6_0/6-0-4.py
@@ -0,0 +1,12 @@
+import pandas as pd
+with open("C:/Users/Даша/Desktop/6_0_2.txt", "w") as f:
+  df = pd.read_csv("C:/Users/Даша/Downloads/wild_boars.csv")
+  col = list(df.columns)
+  for i in col[2:]:  #прохожу по столбцам
+        mod =  df[i].mode()
+        parts = i.split('_')
+        if len(parts) == 2:
+            param = parts[0] 
+        else:
+            param = parts[0] + ' ' + parts[1]
+        print(f'Boars mode {param} is {mod:.2f} {parts[-1]}\n', file=f)
