203d1942c7ef967750a92f95059ceed72e250f42
Ионова Дарья <ionova.dd@edu.spbstu.ru>
2026-05-19 23:48:18 +0300

    Update 6-0-3.py

--- projects_6/task_6_0/6-0-3.py
+++ projects_6/task_6_0/6-0-3.py
@@ -1,5 +1,5 @@
 import pandas as pd
-with open("C:/Users/Даша/Desktop/6_0_3.txt", "w") as f:
+with open("C:/Users/Даша/Desktop/6/6_0_3.txt", "w") as f:
   df = pd.read_csv("C:/Users/Даша/Downloads/wild_boars.csv")
   col = list(df.columns)
   for i in col[2:]:  #прохожу по столбцам
