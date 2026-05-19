2acd93195a11f84c929153d668c6614dcfe84b28
Ионова Дарья <ionova.dd@edu.spbstu.ru>
2026-05-19 23:47:42 +0300

    Create 6-0-6.py

---
+++ projects_6/task_6_0/6-0-6.py
@@ -0,0 +1,8 @@
+import pandas as pd
+with open("C:/Users/Даша/Desktop/6/6_0_6.txt", "w") as f:
+    df = pd.read_csv("C:/Users/Даша/Downloads/wild_boars.csv")
+    q1 = df.groupby('gender')['length_cm'].quantile(0.25)
+    q3 = df.groupby('gender')['length_cm'].quantile(0.75)
+    iqr = q3-q1
+    for gender in iqr.index: #автоматичски создастся индекс из-за групбай, те тут это мэйл фемэйл
+        print(f"IQR for {gender}: {iqr[gender]:.2f} cm", file=f)
