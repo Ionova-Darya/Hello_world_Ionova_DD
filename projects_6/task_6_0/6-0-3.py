a656b701e17420f6f7d3c4a98f7cb2e9221a53d2
Ионова Дарья <ionova.dd@edu.spbstu.ru>
2026-05-19 23:22:09 +0300

    Update 6-0-3.py

--- projects_6/task_6_0/6-0-3.py
+++ projects_6/task_6_0/6-0-3.py
@@ -1,1 +1,12 @@
-l
+import pandas as pd
+with open("C:/Users/Даша/Desktop/6_0_3.txt", "w") as f:
+  df = pd.read_csv("C:/Users/Даша/Downloads/wild_boars.csv")
+  col = list(df.columns)
+  for i in col[2:]:  #прохожу по столбцам
+        medi =  df[i].median()
+        parts = i.split('_')
+        if len(parts) == 2:
+            param = parts[0] 
+        else:
+            param = parts[0] + ' ' + parts[1]
+        print(f'Boars median {param} is {medi:.2f} {parts[-1]}\n', file=f)
