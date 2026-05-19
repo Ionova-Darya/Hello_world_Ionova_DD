b63e6002ee5ca0db2197888cfbcc92c9a535087f
Ионова Дарья <ionova.dd@edu.spbstu.ru>
2026-05-19 23:59:20 +0300

    Create 6-0-8.py

---
+++ projects_6/task_6_0/6-0-8.py
@@ -0,0 +1,5 @@
+import pandas as pd
+with open("C:/Users/Даша/Desktop/6/6_0_8.txt", "w") as f:
+    df = pd.read_csv("C:/Users/Даша/Downloads/wild_boars.csv")
+    cv = (df.groupby('gender')['tusk_length_cm'].std() / df.groupby('gender')['tusk_length_cm'].mean()) * 100
+    print(f'Coefficient of variations:\n{cv}', file=f)
